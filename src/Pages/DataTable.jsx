44e2fa8c38d524ca643c64f091568086bdd23bd8
Yogesh Negi <113903081+itsyogeshnegi@users.noreply.github.com>
2024-03-17 13:03:14 +0530

    done

--- src/Pages/DataTable.jsx
+++ src/Pages/DataTable.jsx
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from "react";
 import { get, ref } from "firebase/database";
 import { getDatabase } from "firebase/database";
 import { app } from "../firebase";
+import { getAuth, signOut } from "firebase/auth"; // Import getAuth and signOut from firebase/auth
 import Table from "react-bootstrap/Table";
 import "bootstrap/dist/css/bootstrap.min.css";
-import * as XLSX from "xlsx"; // Import xlsx library
+import { useNavigate } from "react-router-dom";
+import * as XLSX from "xlsx";
 
 const database = getDatabase(app);
+const auth = getAuth(app); // Get the auth instance from firebase
 
 const DataTable = () => {
   const [users, setUsers] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(10);
+  const navigate = useNavigate(); // Move useNavigate inside the component
 
   useEffect(() => {
     const usersRef = ref(database);
@@ -66,9 +70,21 @@ const DataTable = () => {
   // Change page
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
+  const signOutHandle = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/admin"); // Assuming navigate function exists to redirect the user
+      })
+      .catch(error => {
+        // An error happened.
+        console.error("Error signing out:", error);
+      });
+  };
+
   return (
     <div className="h-screen w-full flex flex-col  items-center">
-      <div className="h-20 w-full bg-slate-300 flex justify-around items-center">
+      <div className="h-20 w-full bg-slate-700 flex justify-around items-center">
         <img src="/dashboard.png" className="h-[70%]" />
         <input
           type="search"
@@ -78,10 +94,16 @@ const DataTable = () => {
           className="w-80 rounded-xl text-center  h-10"
         />
         <button
-          className="bg-green-500 w-16 font-bold h-8 rounded-lg flex justify-evenly items-center"
+          className="bg-green-500 w-8 font-bold h-8 rounded-lg flex justify-evenly items-center"
           onClick={handleExcelDownload}
           title="Download Excel">
           <img src="/download.png" className="h-[80%]" />
+        </button>
+        <button
+          className="bg-transparent w-8 font-bold h-8 rounded-lg flex justify-evenly items-center"
+          onClick={signOutHandle}
+          title="Sign Out">
+          <img src="/log-out.png" className="h-[100%]" />
         </button>
       </div>
       <div className="w-3/4 mt-10">
